7ea95749d32186061b139ecdefafe0335532b746
Kayric <98443692+Kayric@users.noreply.github.com>
2022-04-04 14:21:09 +0700

    Remove about

--- madebyreact/src/component/Navbar.js
+++ madebyreact/src/component/Navbar.js
@@ -17,9 +17,6 @@ function Navbar() {
               <a href="#portfolio">Portfolio</a>
             </li>
             <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
               <a href="#contact">Contact</a>
             </li>
           </ul>
