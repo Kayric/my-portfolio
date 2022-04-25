b912f4e5e133e0c623e338fc0db55d7d42029941
Kayric <98443692+Kayric@users.noreply.github.com>
2022-04-25 09:14:58 +0700

    redirect resume as PDF

--- madebyreact/src/component/Navbar.js
+++ madebyreact/src/component/Navbar.js
@@ -19,10 +19,7 @@ function Navbar() {
             <li>
               <a href="#contact">Contact</a>
             </li>
-            <a
-              href="https://www.canva.com/design/DAEIqwm1aEM/BjzRtqwRfY2JXK-hO8lcWw/view?utm_content=DAEIqwm1aEM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
-              className="btn"
-            >
+            <a href="./img/Kirkrapee2022.pdf" className="btn">
               MY CV
             </a>
           </ul>
